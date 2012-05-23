87ff24c1d03238433487b76a817c95f8a41cfc02
Prashant Singh <prashant@codeforamerica.org>
2012-05-23 13:44:37 -0700

    Switch to dev server for main branch

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 //var BASEURL = 'http://localhost:5000';
-var BASEURL = 'http://surveydet.herokuapp.com';
+var BASEURL = 'http://surveyapidev.herokuapp.com';
 //var surveyid = 'b53eed70-9337-11e1-9bf5-39dee61cc65b';
 var surveyid = '23206450-a0ac-11e1-ae6a-a17fba15c6fd';
 
